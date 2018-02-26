213bb4be2bcbc821ae0615f66c0df26c59863adb
hlozancic <hlozancic@gmail.com>
2018-02-26 11:48:24 +0100

    edited slug trait

--- app/Models/Traits/Slugify.js
+++ app/Models/Traits/Slugify.js
@@ -15,7 +15,7 @@ class Slugify {
             slugOptions: {
                 replacement: '-',               // replace spaces with replacement
                 symbols: true,                  // replace unicode symbols or not
-                remove: null,                   // (optional) regex to remove characters
+                remove: /[.]/g,                 // (optional) regex to remove characters
                 lower: true,                    // result in lower case
                 charmap: slug.charmap,          // replace special characters
                 multicharmap: slug.multicharmap // replace multi-characters
